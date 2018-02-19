af84c9e9bcd3ffa38c153f55cf8d6e2f2c75b271
7amim <tufaruk7@gmail.com>
2018-02-18 22:50:34 -0500

    Created paddle and ball

---
+++ Assets/Scripts/paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour {
+
+    public float paddleSpeed = 1;
+    public Vector3 playerPos;
+    float yPos;
+
+    /* Use this for initialization
+    void Start () {
+        paddleSpeed = 1;
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+        yPos = gameObject.transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+        playerPos = new Vector3(-15, Mathf.Clamp(yPos, -8, 8), 0);
+        gameObject.transform.position = playerPos; 
+	}
+}
